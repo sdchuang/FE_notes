2d5c2433df6f805a3d2675ac0da58f11728f3488
sdc <sdc@sdcdeMBP.lan>
2022-04-10 11:41:39 +0800

    update

--- algorithm/树/二叉树前序遍历.js
+++ algorithm/树/二叉树前序遍历.js
@@ -1,1 +1,11 @@
-// 二叉树前序遍历
+/**
+ * 二叉树前序遍历
+ * @param {rootNode} root 树根节点 
+ */
+function preOrder(root) {
+  if (root) {
+    console.log(root.val);
+    preOrder(root.left);
+    preOrder(root.right);
+  }
+}
